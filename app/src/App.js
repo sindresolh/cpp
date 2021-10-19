46160a01bdf49115e03a242cb262f06f73c5b483
sindresolh <sindresolh@gmail.com>
2021-10-19 13:05:46 +0200

    Initial commit - working redux

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import {useSelector} from 'react-redux';
+import CounterComponent from './redux/testcomponents/counterComponent';
+
+function App() {
+  const counter = useSelector(state => state.counter)
+  return (
+    <div className="App">
+      <CounterComponent />
+    </div>
+  );
+}
+
+export default App;
