795172bcaa299a922ab33f33547800c702511e64
Sindre Solheim <sindsolh@ntnu.no>
2021-10-27 13:06:10 +0200

    Jobbe med å sette opp strukturen

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,10 @@
 import { useSelector } from 'react-redux';
-import CommunicationHandler from './communication/CommunicationHandler';
-import Counter from './redux/testcomponents/counterComponent';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <MainPage />
     </div>
   );
 }
---
+++ app/src/components/topbar/Topbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Topbar.css';
+
+export default function topbar() {
+  return (
+    <div className="Topbar">
+      <h1>Topbar</h1>
+    </div>
+  );
+}
