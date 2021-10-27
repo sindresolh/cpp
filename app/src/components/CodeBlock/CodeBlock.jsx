7dd7aede90da9247dd67fa2eb16b90a9f4cdd281
Henrik <henrikrn@stud.ntnu.no>
2021-10-27 07:46:22 +0200

    Added drag to CodeBlock component. Test for this is not finished

--- app/src/components/CodeBlock/CodeBlock.jsx
+++ app/src/components/CodeBlock/CodeBlock.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import './CodeBlock.css';
+import { ItemTypes } from '../../utils/itemtypes';
+import { useRef } from 'react';
+import { useDrag } from 'react-dnd'
 
-function getClassName(player, category) {
-  const className = `cb c${category} p${player}`;
-  return className;
-}
 
-function CodeBlock({ id, content, player, category }) {
+function CodeBlock({ id, content, player, category, placement=null}) {
+    const placementRef = useRef(placement)
+    const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
+        type: ItemTypes.CODEBLOCK,
+        item: {id, content, player, placement: placementRef.current}
+    ,
+    collect: (monitor) => ({
+        isDragging: monitor.isDragging(), 
+      })}
+    ))
+      
+
+    let className = isDragging ? `cb c${category} p${player} dragging` : `cb c${category} p${player}`
+
   return (
     <div
+    ref={drag}
       data-testid={`codeBlock-p${player}`}
       id={id}
-      className={getClassName(player, category)}
+      className={className}
     >
       {content}
     </div>
--- app/src/components/CodeBlock/__test__/CodeBlock.test.js
+++ app/src/components/CodeBlock/__test__/CodeBlock.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import CodeBlock from '../CodeBlock';
 import '@testing-library/jest-dom';
 import {
@@ -8,11 +8,17 @@ import {
   sampleBlockP3 as propsP3,
   sampleBlockP4 as propsP4,
 } from './sample-block';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 let codeBlock; // uses props from player 1
 
 beforeEach(() => {
-  codeBlock = render(<CodeBlock {...propsP1} />);
+  codeBlock = render(
+    <DndProvider backend={HTML5Backend}>
+      <CodeBlock {...propsP1} />
+    </DndProvider>
+  );
   codeBlock = codeBlock.getByTestId('codeBlock-p1');
 });
 
@@ -21,11 +27,23 @@ test('can render to screen', () => {
 });
 
 test('block border matches player colors', () => {
-  let codeBlockP2 = render(<CodeBlock {...propsP2} />);
+  let codeBlockP2 = render(
+    <DndProvider backend={HTML5Backend}>
+      <CodeBlock {...propsP2} />
+    </DndProvider>
+  );
   codeBlockP2 = codeBlockP2.getByTestId('codeBlock-p2');
-  let codeBlockP3 = render(<CodeBlock {...propsP3} />);
+  let codeBlockP3 = render(
+    <DndProvider backend={HTML5Backend}>
+      <CodeBlock {...propsP3} />
+    </DndProvider>
+  );
   codeBlockP3 = codeBlockP3.getByTestId('codeBlock-p3');
-  let codeBlockP4 = render(<CodeBlock {...propsP4} />);
+  let codeBlockP4 = render(
+    <DndProvider backend={HTML5Backend}>
+      <CodeBlock {...propsP4} />
+    </DndProvider>
+  );
   codeBlockP4 = codeBlockP4.getByTestId('codeBlock-p4');
 
   expect(codeBlock).toHaveClass('p1');
@@ -35,8 +53,24 @@ test('block border matches player colors', () => {
 });
 
 test('block background color matches code block category', () => {
-  let codeBlockP4 = render(<CodeBlock {...propsP4} />);
+  let codeBlockP4 = render(
+    <DndProvider backend={HTML5Backend}>
+      <CodeBlock {...propsP4} />
+    </DndProvider>
+  );
   codeBlockP4 = codeBlockP4.getByTestId('codeBlock-p4');
   expect(codeBlock).toHaveClass('c1'); // variable category
   expect(codeBlockP4).toHaveClass('c2'); // function category
+});
+
+test.skip('dragging changes opacity ', () => {
+  let someDiv = render(<div data-testid={'someDiv'}>some text</div>);
+  someDiv = someDiv.getByTestId('someDiv');
+
+  expect(codeBlock).not.toHaveClass('dragging');
+  fireEvent.dragStart(codeBlock);
+  fireEvent.dragEnter(someDiv);
+  fireEvent.dragOver(someDiv);
+
+  expect(screen.getByTestId('codeBlock-p1')).toHaveClass('dragging');
 });
