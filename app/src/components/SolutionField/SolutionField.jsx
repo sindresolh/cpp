00b78f380edb13ddf107c93910b19d29e6d3873e
Henrik Rudi Næss <henrikrn@stud.ntnu.no>
2021-11-02 10:38:03 +0100

    Created component. Created test folder and file. Can render to screen

---
+++ app/src/components/SolutionField/SolutionField.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SolutionField() {
+  return <div data-testid='solutionField'></div>;
+}
+
+export default SolutionField;
---
+++ app/src/components/SolutionField/__test__/sample-field.js
@@ -0,0 +1,30 @@
+import { PLAYER, CATEGORY } from '../constants';
+
+export const sampleHandLists = {
+  player1: [
+    {
+      id: 'cb-1',
+      content: 'x = 1',
+      player: PLAYER.P1,
+      category: CATEGORY.VARIABLE,
+    },
+    {
+      id: 'cb-2',
+      content: 'y = 2',
+      player: PLAYER.P1,
+      category: CATEGORY.VARIABLE,
+    },
+    {
+      id: 'cb-3',
+      content: 'z = x + y',
+      player: PLAYER.P1,
+      category: CATEGORY.VARIABLE,
+    },
+    {
+      id: 'cb-4',
+      content: 'print(z)',
+      player: PLAYER.P1,
+      category: CATEGORY.FUNCTION,
+    },
+  ],
+};
