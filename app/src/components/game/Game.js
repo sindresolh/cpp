082a0876e9b0c887d1e41baf28f9931ef68face4
Sindre Solheim <sindsolh@stud.ntnu.no>
2021-11-04 13:46:45 +0100

    fixed layout

--- app/src/components/game/Game.js
+++ app/src/components/game/Game.js
@@ -3,6 +3,7 @@ import './Game.css';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import HandList from '../HandList/HandList';
+import SolutionField from '../SolutionField/SolutionField';
 import {
   sampleHandLists as props,
   sampleField as fieldProps,
@@ -13,7 +14,19 @@ export default function Game() {
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="Game">
-        <HandList codeBlocks={props.player1} player={1} />
+        {/*Player 1 and 2 on the left side*/}
+        <div className="GameLeft">
+          <HandList codeBlocks={props.player1} player={PLAYER.P1} />
+          <HandList codeBlocks={props.player2} player={PLAYER.P2} />
+        </div>
+
+        <SolutionField codeLines={fieldProps.field} />
+
+        {/*Player 3 and 4 on the left side*/}
+        <div className="GameRight">
+          <HandList codeBlocks={props.player3} player={PLAYER.P3} />
+          <HandList codeBlocks={props.player3} player={PLAYER.P4} />
+        </div>
       </div>
     </DndProvider>
   );
