d7f399828072eef5eab2f561d3b713c986cf5bc2
Henrik <henrikrn@stud.ntnu.no>
2021-11-05 13:07:16 +0100

    Player component with tests done.

---
+++ app/src/components/Player/__test__/sample-players.js
@@ -0,0 +1,23 @@
+export const player1 = {
+  playerNo: 1,
+  name: 'Per',
+  codeBlocks: [],
+};
+
+export const player2 = {
+  playerNo: 2,
+  name: 'Lise',
+  codeBlocks: [],
+};
+
+export const player3 = {
+  playerNo: 3,
+  name: 'Aase',
+  codeBlocks: [],
+};
+
+export const player4 = {
+  playerNo: 4,
+  name: 'Kjetil',
+  codeBlocks: [],
+};
--- app/src/components/game/Game.js
+++ app/src/components/game/Game.js
@@ -8,22 +8,22 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import SolutionField from '../SolutionField/SolutionField';
 import HandList from './../HandList/HandList';
+import Player from '../Player/Player';
 export default function Game() {
   return (
     <DndProvider backend={HTML5Backend}>
       <div className='Game'>
         <div className='leftContainer'>
-          {/*/ TODO: lage player komponenter som inneholder handlist */}
-          <HandList codeBlocks={listProps.player1} player={1} />
-          <HandList codeBlocks={listProps.player3} player={3} />
+          <Player playerNo={1} name={'Per'} codeBlocks={listProps.player1} />
+          <Player playerNo={3} name={'Lise'} codeBlocks={listProps.player3} />
         </div>
         <div className='middleContainer'>
           <SolutionField codeLines={fieldProps.field} />
           {/*/ TODO: legge til sidebar */}
         </div>
         <div className='rightContainer'>
-          <HandList codeBlocks={listProps.player2} player={2} />
-          <HandList codeBlocks={listProps.player4} player={4} />
+          <Player playerNo={2} name={'Aase'} codeBlocks={listProps.player2} />
+          <Player playerNo={4} name={'Kjetil'} codeBlocks={listProps.player4} />
         </div>
       </div>
     </DndProvider>
