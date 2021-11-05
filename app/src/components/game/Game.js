3bccbdde3d9d662cdcbff2909d3336dddda07021
Henrik <henrikrn@stud.ntnu.no>
2021-11-05 10:44:00 +0100

    Quick mockup of Game component with solution field and handlists. Uses flex to place elements.

--- app/src/components/game/Game.js
+++ app/src/components/game/Game.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Game.css';
-import Counter from '../../redux/testcomponents/counterComponent';
-
+import {
+  sampleField as fieldProps,
+  sampleHandLists as listProps,
+} from '../../utils/sample-data';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import SolutionField from '../SolutionField/SolutionField';
+import HandList from './../HandList/HandList';
 export default function Game() {
   return (
-    <div className="Game">
-      <h1>Game</h1> {/* TODO : Add the game elements here */}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className='Game'>
+        <div className='leftContainer'>
+          {/*/ TODO: lage player komponenter som inneholder handlist */}
+          <HandList codeBlocks={listProps.player1} player={1} />
+          <HandList codeBlocks={listProps.player3} player={3} />
+        </div>
+        <div className='middleContainer'>
+          <SolutionField codeLines={fieldProps.field} />
+          {/*/ TODO: legge til sidebar */}
+        </div>
+        <div className='rightContainer'>
+          <HandList codeBlocks={listProps.player2} player={2} />
+          <HandList codeBlocks={listProps.player4} player={4} />
+        </div>
+      </div>
+    </DndProvider>
   );
 }
