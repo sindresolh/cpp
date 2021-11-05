8ca03d128586880a79ca7b83eeb8596a86686e09
Henrik <henrikrn@stud.ntnu.no>
2021-11-05 10:44:00 +0100

    Quick mockup of Game component with solution field and handlists. Uses flex to place elements.

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,13 @@
 import { useSelector } from 'react-redux';
 import CommunicationHandler from './communication/CommunicationHandler';
 import Counter from './redux/testcomponents/counterComponent';
-import HandList from './components/HandList/HandList';
-import {
-  sampleHandLists as props,
-  sampleField as fieldProps,
-} from './utils/sample-data';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import SolutionField from './components/SolutionField/SolutionField';
 
 import './App.css';
 import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <MainPage />
     </div>
   );
