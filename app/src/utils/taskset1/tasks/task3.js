37a628f55eae5712524c7d0692f7827380725c0b
Sindre Solheim <sindsolh@stud.ntnu.no>
2021-11-09 12:08:18 +0100

    ny tom task som eksempel

---
+++ app/src/utils/taskset1/tasks/task3.js
@@ -0,0 +1,17 @@
+const handList = {
+  player1: [],
+  player2: [],
+  player3: [],
+  player4: [],
+};
+
+const solutionField = {
+  field: [],
+};
+
+export const task3 = {
+  id: 3,
+  description: 'Her var det ingenting.',
+  handList: handList,
+  solutionField: solutionField,
+};
--- app/src/utils/taskset1/taskset.js
+++ app/src/utils/taskset1/taskset.js
@@ -2,10 +2,11 @@
 
 import { task1 } from './tasks/task1';
 import { task2 } from './tasks/task2';
+import { task3 } from './tasks/task3';
 
 export const taskset = {
   author: 'Sindre Solheim',
   date: new Date(2021, 10, 5), // Note: In javascripts months starts at 0, so december is 11 :)
   description: 'Example task set',
-  tasks: [task1, task2],
+  tasks: [task1, task2, task3],
 };
