300478084c61fe9ee113fc6d4227d75f71ef96bf
Sindre Solheim <sindsolh@stud.ntnu.no>
2021-11-09 14:06:20 +0100

    Går bare til neste task om oppgaven er riktig. For nå er det bare riktig om de ikke endrer på noe

--- app/src/communication/CommunicationHandler.js
+++ app/src/communication/CommunicationHandler.js
@@ -11,6 +11,10 @@ import {
   nextTask,
 } from '../redux/actions';
 import { NEW_COUNT, SET_LIST, SET_FIELD, NEXT_TASK } from './messages';
+import {
+  twoDimensionalArrayIsEqual,
+  arrayIsEqual,
+} from '../utils/compareArrays/compareArrays';
 
 /**
  * Helper function to let us call dispatch from a class function
@@ -165,67 +169,6 @@ class CommunicationHandler extends Component {
     );
   }
 }
-/**
- * Shallow equal check on two dimentional array
- * @param {array} arr1 previous state
- * @param {array} arr2  payload state
- * @returns true the arrays within the 2D array is equal
- */
-const twoDimensionalArrayIsEqual = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-
-  for (var i = 0; i < arr1.length; i++) {
-    if (!arrayIsEqual(arr1[i], arr2[i])) return false;
-  }
-
-  return true;
-};
-
-/**
- * Shallow equal check on array
- * @param {array} arr1 previous state
- * @param {array} arr2 payload state
- * @returns true if all the objects and values within the array is equal
- */
-const arrayIsEqual = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-  for (var i = 0; i < arr1.length; i++) {
-    if (!objectIsEqual(arr1[i], arr2[i])) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-/**
- *  Check if all keys in an object is equal.
- * Since this function is used recursively, check if the object is a value.
- *
- * Code is taken from: https://dmitripavlutin.com/how-to-compare-objects-in-javascript/#3-shallow-equality
- * @param {object or value} object1 block or indent value
- * @param {object or value} object2 block or indent value
- * @returns true if all keys in an object are equal
- */
-const objectIsEqual = (object1, object2) => {
-  if (typeof object1 !== 'object') {
-    // not an object, check the values
-    return object1 === object2;
-  }
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (let key of keys1) {
-    if (!objectIsEqual(object1[key], object2[key])) {
-      return false;
-    }
-  }
-  return true;
-};
 
 export default connect(
   mapStateToProps,
--- app/src/components/Sidebar/Sidebar.js
+++ app/src/components/Sidebar/Sidebar.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarButton from './SidebarButton/SidebarButton';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { nextTask, newTaskShoutEvent } from '../../redux/actions';
+import { arrayIsEqual } from '../../utils/compareArrays/compareArrays';
 
 export default function Sidebar() {
   const dispatch = useDispatch();
 
+  const field = useSelector((state) => state.solutionField);
+  const currentTask = useSelector((state) => state.currentTask);
+  let currentTaskNumber = currentTask.currentTaskNumber;
+  let currentTaskObject = currentTask.tasks[currentTaskNumber];
+
+  /**
+   * Make all players go to the next task of the submit is correct
+   */
   const handleSubmit = () => {
-    dispatch(nextTask());
-    dispatch(newTaskShoutEvent());
+    if (arrayIsEqual(field, currentTaskObject.solutionField.correct)) {
+      dispatch(nextTask());
+      dispatch(newTaskShoutEvent());
+    }
   };
   return (
     <div className="Sidebar">
