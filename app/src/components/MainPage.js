c5132ca900ce8f99f907eba5776117f21912100a
sindresolh <52532163+sindresolh@users.noreply.github.com>
2022-01-10 12:16:58 +0100

    Update MainPage.js

--- app/src/components/MainPage.js
+++ app/src/components/MainPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './MainPage.css';
-import Topbar from './Topbar/Topbar';
+import Topbar from './topbar/Topbar';
 import Game from './Game/Game';
 
 export default function MainPage() {
