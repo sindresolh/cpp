61ccc94ad78a85c1f24831c4346b78a67a09b2e8
Sindre Solheim <sindsolh@stud.ntnu.no>
2022-01-12 12:29:22 +0100

    typo

--- app/src/components/MainPage.js
+++ app/src/components/MainPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './MainPage.css';
-import Topbar from './Topbar/Topbar';
-import Game from './Game/Game';
+import Topbar from './topbar/Topbar';
+import Game from './game/Game';
 
 export default function MainPage() {
   return (
