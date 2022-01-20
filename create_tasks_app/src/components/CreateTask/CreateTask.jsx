d7dd0502cc0b4ff007be8009f7f4099f51f58067
Henrik Rudi Næss <henrik@Henriks-MacBook-Air.local>
2022-01-20 10:10:12 +0100

    Added description and hints

--- create_tasks_app/src/components/CreateTask/CreateTask.jsx
+++ create_tasks_app/src/components/CreateTask/CreateTask.jsx
@@ -8,6 +8,21 @@ import './prism.css';
 import './CreateTask.css';
 import { useNavigate } from 'react-router-dom';
 
+const Hint = (props) => {
+  const number = props.number;
+  return (
+    <div
+      className='textAreaContainer'
+      id={`hint-${number}`}
+      key={`hint-${number}`}
+    >
+      <div className='textAreaFlex' key={`hint-${number}`}>
+        <textarea className='hintInput' rows={3} key={`hint-${number}`} />
+      </div>
+    </div>
+  );
+};
+
 /**
  * Includes a code editor to write/add code. This code can be turned into a task.
  * User can properties such as description, hints, amount of attempts.
@@ -16,7 +31,10 @@ import { useNavigate } from 'react-router-dom';
  */
 function CreateTask() {
   const [code, setCode] = useState(SAMPLE_TEXT);
+  const [hints, setHints] = useState([<Hint number={1} />]);
+  const hintLength = hints.length;
   let navigate = useNavigate();
+
   return (
     <div className='container' data-testid={'createTask'}>
       <div className='leftContainer'>
@@ -34,6 +52,38 @@ function CreateTask() {
         />
       </div>
       <div className='rightContainer'>
+        <div className='textAreaContainer'>
+          <div className='textAreaFlex'>
+            <textarea
+              className='textarea'
+              rows={5}
+              placeholder='Description for the task'
+              id='description'
+            />
+          </div>
+        </div>
+        <div className='hints'>
+          <h3>Hints</h3>
+          {hints}
+          <button
+            onClick={() =>
+              setHints([...hints, <Hint number={hints.length + 1} />])
+            }
+            disabled={hints.length > 4} // max 5 hints for now
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              let updatedHints = [...hints];
+              updatedHints.pop();
+              setHints([...updatedHints]);
+            }}
+            disabled={hints.length < 2}
+          >
+            -
+          </button>
+        </div>
         <button className='cancelButton' onClick={() => navigate('/')}>
           Cancel
         </button>
