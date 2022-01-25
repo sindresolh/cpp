690cce16083f06d6faa0cfe276f989fd6a5807c2
Sindre Solheim <sindsolh@stud.ntnu.no>
2022-01-25 12:18:07 +0100

    grenbrukt sidebarbutton

--- app/src/components/Lobby/Lobby.js
+++ app/src/components/Lobby/Lobby.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import SidebarButton from '../Sidebar/SidebarButton/SidebarButton';
+import SubmitIcon from '../../images/buttonIcons/submit.png';
+import { COLORS } from '../../utils/constants';
 
 function Lobby({ handleClick }) {
   return (
-    <>
+    <div style={{ position: 'absolute', top: '10%', left: '20%' }}>
       <h1>Lobby</h1>
-      <button onClick={handleClick}>Start game</button>
-    </>
+      <SidebarButton
+        title='Start game'
+        icon={SubmitIcon}
+        color={COLORS.lightgreen}
+        handleClick={handleClick}
+        width='9em'
+      />
+    </div>
   );
 }
 
--- app/src/components/Sidebar/SidebarButton/SidebarButton.js
+++ app/src/components/Sidebar/SidebarButton/SidebarButton.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './SidebarButton.css';
 
-export default function SidebarButton({ title, icon, color, handleClick }) {
+export default function SidebarButton({
+  title,
+  icon,
+  color,
+  handleClick,
+  width = '7em',
+}) {
   return (
     <button
       onClick={handleClick}
-      style={{ background: color }}
+      style={{ background: color, width: width }}
       className='SideBarButton'
     >
       <img height='30' width='auto' src={icon} alt='Icon' />
