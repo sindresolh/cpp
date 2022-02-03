1532fa7850d8bb7452a58ff382dbfa005efda968
Sindre Solheim <sindsolh@stud.ntnu.no>
2022-02-03 10:57:08 +0100

    added some more python exercises

---
+++ python_exercises/easy/CountEmma.py
@@ -0,0 +1,14 @@
+# Source: https://pynative.com/python-basic-exercise-for-beginners/
+
+# Description: Write a program to find how many times substring “Emma” appears in the given string.
+
+# Hint: Given "Emma is good developer. Emma is a writer" the function should return 2.
+# Hint: Use string method count()
+
+str = "Emma is good developer. Emma is a writer"
+answ = str.count("Emma")
+print(answ)
+
+#$answ = "Emma".count(str)
+#$answ = "Emma".get(str)
+#$answ = str.get(Emma)
