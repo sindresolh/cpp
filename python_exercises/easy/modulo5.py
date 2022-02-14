febf7d4c4e213ac8c4e35ff7e55d2185510b8bc9
Sindre Solheim <sindsolh@stud.ntnu.no>
2022-02-14 10:11:55 +0100

    oppdatert easytask

--- python_exercises/easy/modulo5.py
+++ python_exercises/easy/modulo5.py
@@ -3,6 +3,8 @@
 # Description: Iterate the given list of numbers and print only those numbers which are divisible by 5
 
 # Hint: % (modulo) is a math operation that finds the remainder when one integer is divided by another
+# Hint For example, 16 % 4 = 0, since if we divide 16 by 4, we get 4 with remainder 0.
+# Hint: For example, 17 % 5 = 2, since if we divide 17 by 5, we get 3 with remainder 2.
 
 num_list = [10, 33, 55]
 for num in num_list:
