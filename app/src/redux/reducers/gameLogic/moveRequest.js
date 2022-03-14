65ddce8aca873fa30d9f913b6d01e83c9166d2b1
Henrik Rudi Næss <henrik@Henriks-MacBook-Air.local>
2022-03-14 11:45:59 +0100

    Updated doc in move request

--- app/src/redux/reducers/gameLogic/moveRequest.js
+++ app/src/redux/reducers/gameLogic/moveRequest.js
@@ -9,9 +9,11 @@ const initialState = {
 
 /**  Reducer for storing the last move request a player has done.
  *
- * TODO
  * Example of a request:
- * { id, fromPos, toPos, ... }
+ * { '1', 2, 0, 'SF' }
+ * This means: 'move block with id '1' to index 2 at indent 0 in the solution field.
+ * If a player requested to move a block in their hand the field property would say either '1', '2', '3' or '4'
+ * depending on who requested.
  *
  * @param {*} state : object with move request information
  * @param {*} action
