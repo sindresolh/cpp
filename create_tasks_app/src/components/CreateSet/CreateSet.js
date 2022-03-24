780f33efdb00e736ff6a958c6bf21c066307070c
Henrik Rudi Næss <henrik@Henriks-MacBook-Air.local>
2022-03-24 13:04:35 +0100

    Added logic for reading multiple files

--- create_tasks_app/src/components/CreateSet/CreateSet.js
+++ create_tasks_app/src/components/CreateSet/CreateSet.js
@@ -1,17 +1,56 @@
-import React from 'react';
 import './CreateSet.css';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
-const sampleTasks = [
-  { title: 'sumlist' },
-  { title: 'createFunction' },
-  { title: 'whileLoop' },
-];
-
+/**
+ * Read files locally and combine them into a tasks set.
+ *
+ * Credit for multiple-file-reading logic: https://stackoverflow.com/a/62424373
+ */
 function CreateSet() {
   let navigate = useNavigate();
-  const [tasks, setTasks] = useState(sampleTasks);
+  const [tasks, setTasks] = useState([]);
+
+  const readFileContents = async (file) => {
+    return new Promise((resolve, reject) => {
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = reject;
+      fileReader.readAsText(file);
+    });
+  };
+  const readAllFiles = async (allFiles) => {
+    const results = await Promise.all(
+      allFiles.map(async (file) => {
+        const fileContents = {
+          name: file.name,
+          content: await readFileContents(file),
+        };
+        return fileContents;
+      })
+    );
+    return results;
+  };
+
+  const handleUpload = (e) => {
+    let allFiles = [];
+    [...e.target.files].map((file) => allFiles.push(file));
+
+    readAllFiles(allFiles)
+      .then((result) => {
+        setTasks([...result]);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  const saveTaskSet = () => {
+    const contents = tasks.map((task) => task.content);
+    console.log(contents);
+  };
 
   return (
     <div className='container2'>
@@ -19,24 +58,33 @@ function CreateSet() {
       <h5>
         Add tasks located on your computer to combine them into a task set.
       </h5>
+      <h6>Use Command/Ctr + Click to select multiple tasks.</h6>
       <ul className='tasks'>
         {tasks.map((task, index) => (
           <li className='task' key={'task ' + index + 1}>{`Task ${
             index + 1
-          } | ${task.title}`}</li>
+          } | ${task.name}`}</li>
         ))}
       </ul>
+      <div style={{ margin: '5vh' }}>
+        <form>
+          <label style={{ color: 'black' }}>
+            <input
+              type='file'
+              style={{ color: 'black', fontSize: '2vh' }}
+              name='tasks'
+              multiple='multiple'
+              onChange={handleUpload}
+            />
+          </label>
+        </form>
+      </div>
       <div className='buttonDiv'>
         <button className='button cancel' onClick={() => navigate('/')}>
           Cancel
         </button>
-        <button
-          className='button save'
-          onClick={() => {
-            console.log('save');
-          }}
-        >
-          Save
+        <button className='button save' onClick={() => saveTaskSet()}>
+          Save task set
         </button>
       </div>
     </div>
